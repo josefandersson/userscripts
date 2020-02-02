b5a7971c95bdc0d6642bc8077056945d4df7818e
Josef <josefandman@gmail.com>
2020-02-02 17:49:29 +0100

    Syncs between tabs

--- HF-title-alerts/hf-title-alerts.user.js
+++ HF-title-alerts/hf-title-alerts.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         HF Title Alerts
 // @namespace    https://github.com/josefandersson/userscripts/tree/master/HF-title-alerts
-// @version      1.0
+// @version      1.1
 // @description  Adds an alert indicator to the page title.
 // @author       DrDoof
 // @icon         https://hackforums.net/favicon.ico
 // @match        https://hackforums.net/*
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @grant        GM_addValueChangeListener
 // @run-at       document-end
 // ==/UserScript==
 
@@ -20,10 +23,26 @@ unsafeWindow.Notify.socket.on('notify_notice', data => {
 
 document.querySelector('.notifycp').addEventListener('click', updateTitle)
 
+let lastAlertUpdate = 0
+
 function updateTitle() {
     let prefix = ''
     if (unsafeWindow.Notify.alerts > 0) {
         prefix = `(${unsafeWindow.Notify.alerts}) `
+    } else {
+        GM.setValue('clearalerts', Date.now())
     }
     document.title = document.title.replace(/^(\(\d+\) )?(.*)/, `${prefix} $2`)
+    lastAlertUpdate = Date.now()
 }updateTitle()
+
+try {
+    GM_addValueChangeListener('clearalerts', (key, oldVal, newVal, isRemote) => {
+        if (isRemote) {
+            if (lastAlertUpdate < newVal) {
+                unsafeWindow.Notify.alerts = 0
+                updateTitle()
+            }
+        }
+    })
+} catch(e) {}
